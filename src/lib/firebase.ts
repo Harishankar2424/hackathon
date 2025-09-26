a146e39fc1a55b5ad37ac68931e8d8f1932d5617
Harishankar S <harishankar150423@gmail.com>
2025-09-26 23:21:35 +0000

    <script type="module">   // Import the functions you need from the SDKs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,13 @@
 import { initializeApp, getApp, getApps } from 'firebase/app';
 
 const firebaseConfig = {
-  "projectId": "studio-6374486714-22931",
-  "appId": "1:532489106407:web:3a7eb2a69748eb2d82c342",
-  "apiKey": "AIzaSyAcfVxGi0pjmqqWvN1yTYkkmH0DYEqa2gI",
-  "authDomain": "studio-6374486714-22931.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "532489106407"
-};
+    apiKey: "AIzaSyAcfVxGi0pjmqqWvN1yTYkkmH0DYEqa2gI",
+    authDomain: "studio-6374486714-22931.firebaseapp.com",
+    projectId: "studio-6374486714-22931",
+    storageBucket: "studio-6374486714-22931.firebasestorage.app",
+    messagingSenderId: "532489106407",
+    appId: "1:532489106407:web:55b8d6e3867744b682c342"
+  };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
